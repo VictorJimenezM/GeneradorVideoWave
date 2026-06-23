5a365ffec7fc8d543c1be71a3fd53870fcc74c1d
VictorJimenezM <charlyjm@gmail.com>
2026-06-22 20:54:15 -0400

    Feat: Mejoras UIX

--- src/components/AudioVisualizer.tsx
+++ src/components/AudioVisualizer.tsx
@@ -1305,7 +1305,7 @@ export default function AudioVisualizer() {
               </button>
 
               <div className="text-xs text-slate-400">
-              IMPORTANTE: Esperar en PRIMER PLANO a que termine la reproducción de audio para descargar el video cuadrado de 1080x1080 px.
+              IMPORTANTE: Esperar en PRIMER PLANO para que capture CORRECTAMENTE el video, terminado el audio empezara la descarga automatica.
               </div>
 
               {isExporting ? (
